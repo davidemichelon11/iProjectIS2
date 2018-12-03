1cbd638ddf99ecf70ed86d397efc693d1a48d501
davidemichelon11 <davide.michelon-1@studenti.unitn.it>
2018-12-03 14:55:54 +0100

    add test cases fix: #15

--- app.test.js
+++ app.test.js
@@ -1,5 +1,9 @@
 const app     = require('./app')
-
+const examsRoute = require('./app/routes/examsRoute')
 test('app should be defined',()=>{
     expect(app).toBeDefined();
+})
+
+test('route of exam should be defined', ()=>{
+    expect(examsRoute).toBeDefined();
 })
--- app/models/exam.test.js
+++ app/models/exam.test.js
@@ -1,15 +1,17 @@
 const request = require('supertest'); //allow testing w/ good syntax
 const app     = require('../../app')
 
-///GET work
-test('GET / should return 200', async () => {
-    const response = await request(app).get('/v1/exams');
-    expect(response.statusCode).toBe(200);
-});
+
 
 describe('CRUD on /v1/exams', () => {
+  ///GET work
+  test('GET / should return 200', async () => {
+    const response = await request(app).get('/v1/exams');
+    expect(response.statusCode).toBe(200);
+  });
+
   const Exam = require('../models/exam');  
-  test('POST v1/exams ', async () => {
+  test('POST, DELETE and PUT on v1/exams ', async () => {
     const response = await request(app).post('/v1/exams/')
                                         .send({
                                           name: 'Analisi 1',
@@ -19,5 +21,32 @@ describe('CRUD on /v1/exams', () => {
                                           examType: 'Oral'
                                         });
     expect(response.status).toBe(201);
+
+    answer = await request(app).get('/v1/exams');
+    answer = JSON.stringify(answer)
+    answer = JSON.parse(answer)
+    answer = JSON.parse(answer.text)
+    var url = '/v1/exams/' + answer[0].id
+
+    const PUTresp = await request(app).put(url)
+                                        .send({
+                                          name:'Base di dati'
+                                        })
+    expect(PUTresp.status).toBe(200)
+
+    const resp = await request(app).delete(url)
+    expect(resp.status).toBe(204);
+
   });
+  test('DELETE v1/exams should return 404', async()=>{
+    var url = '/v1/exams/'+123432;
+    const resp = await request(app).delete(url)
+    expect(resp.status).toBe(404);
+  })
+  test('POST v1/exams should return 403 ', async () => {
+    const response = await request(app).post('/v1/exams')
+                                        .send({
+                                            name: 'Analisi'                                        });    
+    expect(response.status).toBe(403);
+  });  
 });
